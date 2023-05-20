70fd87a44ae4736091066275c94be63fc6d0618b
ronaldruan7 <127566812+ronaldruan7@users.noreply.github.com>
2023-05-19 22:30:10 -0300

    urls.py
    Create

---
+++ urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path, include
+from cabeleleiroapp.views import index
+from cabeleleiroapp.views import geracode
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('index/', index, name='index'),
+    path('', include('usuarios.urls')),
+    path('geracode/', geracode, name='geracode'),
+]
